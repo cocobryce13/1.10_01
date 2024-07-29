464927b83d025830933a27091bb91cdd3eb15fa2
sokol <sokol@ASUS>
2024-07-29 22:07:42 +0500

    Добавьте файлы проекта.

---
+++ 1.10_01.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int size;
+    int a;
+    std::cout << "Введите размер массива: ";
+    std::cin >> size;
+
+    int* arr = new int[size] {};
+
+    for (int i = 0; i < size; i++)
+    {
+        int a;
+        std::cout << "arr [" << i << "] = ";
+        std::cin >> a;
+        arr[i] = a;
+    }
+
+    std::cout << "Введёный массив: ";
+
+
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << "\n\n\n";
+    delete[] arr;
+
+    return 0;
+}
